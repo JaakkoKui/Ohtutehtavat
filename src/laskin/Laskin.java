5e2b7f7a608db470bb83f541ccb22570454cfe56
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2022-09-18 13:48:27 +0300

    Svn ei toimi, mutta testit toimii.

---
+++ src/laskin/Laskin.java
@@ -0,0 +1,51 @@
+package laskin;
+import java.io.Console;
+import java.lang.Math;
+public class Laskin {
+
+    private double tulos;  	// Muuttuja tulokselle
+
+    public void nollaa() {  // Nollaa tulosmuuttuja
+        tulos = 0;
+    }
+
+    public double annaTulos() {
+        return tulos;
+    }
+
+    public void lisaa(double n) {
+        tulos = tulos + n;
+    }
+
+    public void vahenna(double n) {
+        tulos = tulos - n;
+    }
+
+    public void kerro(double n) {
+        tulos = tulos * n;
+    }
+
+    public void jaa(double n) {
+    	if (n==0) throw new IllegalArgumentException("Nollalla ei voi jakaa");
+        tulos = tulos / n;
+    }
+    
+    public void nelio(double n) {
+    	if (n < 0) throw new IllegalArgumentException("Negatiivisella luvulla ei ole neliöjuurta");
+        tulos = n * n;
+    }
+
+    public void neliojuuri(double n) {
+    	tulos = (int) Math.sqrt(tulos);
+    }
+
+    public void virtaON() {
+        nollaa();
+        tulos = 0;
+    }
+
+    public void virtaOFF() {
+        System.out.println("Moikka");
+    }
+
+}
